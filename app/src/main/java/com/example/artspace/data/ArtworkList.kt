502f8a65c19ce6264fdf5d8f2c1f6eb5fae39aac
heather7283 <heather7283@protonmail.com>
2026-02-23 03:40:44 +0400

    feat(data): add more artworks

--- app/src/main/java/com/example/artspace/data/ArtworkList.kt
+++ app/src/main/java/com/example/artspace/data/ArtworkList.kt
@@ -10,6 +10,30 @@ class ArtworkList {
             authorRes = R.string.artwork1_artist,
             year = 1831,
         ),
+        Artwork(
+            imageRes = R.drawable.artwork2,
+            titleRes = R.string.artwork2_title,
+            authorRes = R.string.artwork2_artist,
+            year = 1665,
+        ),
+        Artwork(
+            imageRes = R.drawable.artwork3,
+            titleRes = R.string.artwork3_title,
+            authorRes = R.string.artwork3_artist,
+            year = 1872,
+        ),
+        Artwork(
+            imageRes = R.drawable.artwork4,
+            titleRes = R.string.artwork4_title,
+            authorRes = R.string.artwork4_artist,
+            year = 1818,
+        ),
+        Artwork(
+            imageRes = R.drawable.artwork5,
+            titleRes = R.string.artwork5_title,
+            authorRes = R.string.artwork5_artist,
+            year = 1887,
+        ),
     )
 
     fun getArtworkCount(): Int = artworks.size
