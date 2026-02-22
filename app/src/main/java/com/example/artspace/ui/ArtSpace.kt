95a9608fd745e3119815b39aaa0b11a9b8995867
heather7283 <heather7283@protonmail.com>
2026-02-23 03:41:46 +0400

    feat(ui): navigation between artworks

--- app/src/main/java/com/example/artspace/ui/ArtSpace.kt
+++ app/src/main/java/com/example/artspace/ui/ArtSpace.kt
@@ -1,19 +1,27 @@
-package com.example.artspace.ui
+ package com.example.artspace.ui
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
@@ -23,6 +31,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 import com.example.artspace.R
@@ -31,48 +40,74 @@ import com.example.artspace.data.ArtworkList
 
 val artworkList = ArtworkList()
 
+ @Preview(locale = "ru")
+ @Composable
+ fun ArtSpacePreview() {
+     val configuration = LocalConfiguration.current
+     val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+     val currentIndex = rememberSaveable { mutableIntStateOf(0) }
+
+     ArtSpaceTheme() {
+         Surface() {
+             val padding = dimensionResource(R.dimen.edges_padding)
+             if (isLandscape) {
+                 LandscapeLayout(currentIndex, Modifier.safeContentPadding().padding(padding))
+             } else {
+                 PortraitLayout(currentIndex, Modifier.safeContentPadding().padding(padding))
+             }
+         }
+     }
+ }
+
 @Composable
 fun ArtSpace() {
     val configuration = LocalConfiguration.current
     val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+    val currentIndex = rememberSaveable { mutableIntStateOf(0) }
 
     ArtSpaceTheme() {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
+        Surface() {
             val padding = dimensionResource(R.dimen.edges_padding)
-
             if (isLandscape) {
-                LandscapeLayout(Modifier.safeContentPadding().padding(padding))
+                LandscapeLayout(currentIndex, Modifier.safeContentPadding().padding(padding))
             } else {
-                PortraitLayout(Modifier.safeContentPadding().padding(padding))
+                PortraitLayout(currentIndex, Modifier.safeContentPadding().padding(padding))
             }
         }
     }
 }
 
 @Composable
-fun PortraitLayout(modifier: Modifier = Modifier) {
+fun PortraitLayout(currentIndex: MutableState<Int>, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        val artwork = artworkList.getArtworkAt(0)
+        val artwork = artworkList.getArtworkAt(currentIndex.value)
         ArtworkImage(artwork)
         Spacer(Modifier.size(dimensionResource(R.dimen.spacing)))
         ArtworkInfo(artwork)
+        Spacer(Modifier.weight(1f))
+        ControlButtons(currentIndex, modifier.fillMaxWidth())
     }
 }
 
 @Composable
-fun LandscapeLayout(modifier: Modifier = Modifier) {
+fun LandscapeLayout(currentIndex: MutableState<Int>, modifier: Modifier = Modifier) {
     Row(
         modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
     ) {
-        val artwork = artworkList.getArtworkAt(0)
+        val artwork = artworkList.getArtworkAt(currentIndex.value)
         ArtworkImage(artwork)
         Spacer(Modifier.size(dimensionResource(R.dimen.spacing)))
-        ArtworkInfo(artwork)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            ArtworkInfo(artwork)
+            Spacer(Modifier.weight(1f))
+            ControlButtons(currentIndex, modifier.fillMaxWidth())
+        }
     }
 }
 
@@ -99,6 +134,7 @@ fun ArtworkInfo(artwork: Artwork, modifier: Modifier = Modifier) {
         Text(
             text = stringResource(artwork.titleRes),
             fontSize = dimensionResource(R.dimen.title_font_size).value.sp,
+            lineHeight = dimensionResource(R.dimen.title_font_size).value.sp,
             textAlign = TextAlign.Center,
             fontWeight = FontWeight.Bold,
             color = MaterialTheme.colorScheme.primary,
@@ -107,6 +143,7 @@ fun ArtworkInfo(artwork: Artwork, modifier: Modifier = Modifier) {
         Text(
             text = stringResource(artwork.authorRes),
             fontSize = dimensionResource(R.dimen.author_font_size).value.sp,
+            lineHeight = dimensionResource(R.dimen.author_font_size).value.sp,
             textAlign = TextAlign.Center,
             color = MaterialTheme.colorScheme.secondary,
         )
@@ -114,8 +151,37 @@ fun ArtworkInfo(artwork: Artwork, modifier: Modifier = Modifier) {
         Text(
             text = artwork.year.toString(),
             fontSize = dimensionResource(R.dimen.year_font_size).value.sp,
+            lineHeight = dimensionResource(R.dimen.year_font_size).value.sp,
             textAlign = TextAlign.Center,
             color = MaterialTheme.colorScheme.tertiary,
         )
+    }
+}
+
+@Composable
+fun ControlButtons(currentIndex: MutableState<Int>, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        // Previous
+        Button(
+            onClick = { currentIndex.value-- },
+            enabled = currentIndex.value > 0,
+        ) {
+            Text(
+                text = stringResource(R.string.prev_button_text),
+            )
+        }
+        // Next
+        Button(
+            onClick = { currentIndex.value++ },
+            enabled = currentIndex.value < artworkList.getArtworkCount() - 1,
+        ) {
+            Text(
+                text = stringResource(R.string.next_button_text),
+            )
+        }
     }
 }
