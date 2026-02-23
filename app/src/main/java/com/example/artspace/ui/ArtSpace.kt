4040bcdeebcd48f77c11d5b8487e994e88a9ac10
heather7283 <heather7283@protonmail.com>
2026-02-23 04:00:48 +0400

    chore: optimise imports

--- app/src/main/java/com/example/artspace/ui/ArtSpace.kt
+++ app/src/main/java/com/example/artspace/ui/ArtSpace.kt
@@ -2,12 +2,10 @@
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
@@ -19,13 +17,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.debugInspectorInfo
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -33,12 +28,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import com.example.artspace.ui.theme.ArtSpaceTheme
 import com.example.artspace.R
 import com.example.artspace.data.Artwork
 import com.example.artspace.data.ArtworkList
+import com.example.artspace.ui.theme.ArtSpaceTheme
 
-val artworkList = ArtworkList()
+ val artworkList = ArtworkList()
 
  @Preview(locale = "ru")
  @Composable
